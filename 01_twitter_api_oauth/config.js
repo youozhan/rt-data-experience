40f9f31c444ef94fec2c9429c1b8739ddf75a226
Youchun <30670835+youozhan@users.noreply.github.com>
2019-11-21 16:03:24 -0500

    Code examples

---
+++ 01_twitter_api_oauth/config.js
@@ -0,0 +1,12 @@
+// Change this file to config.js
+// Add your keys
+// Add file .gitignore: config.js
+// Load with 
+// var config = require('./config.js');
+
+module.exports = {
+  consumer_key:         'Vp7i4ekozCXjlSXSN3jnzVmvO', 
+  consumer_secret:      '6CDJkVRtgYKkdYJBbVEL1HnlzaqQDoJPkV37qRcKGNc4PLRNcJ',
+  access_token:         '117425060-tK20dawOINYv8AFQnvBZHqTHesUsHIYVfPymHqvm',
+  access_token_secret:  'obq5MiBCMsImP2oMFaT8ktfXftpjX1oQYx9tdD2KThDrp'
+}
